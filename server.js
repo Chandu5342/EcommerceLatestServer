70590fd584ffb4f6a9be942858108639b42ea7d7
Chandu5342 <karrichandu309@gmail.com>
2025-11-06 23:36:10 +0530

    afterauth

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import { errorHandler } from "./middlewares/errorMiddleware.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use("/api/auth", authRoutes);
+
+// error handler
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
